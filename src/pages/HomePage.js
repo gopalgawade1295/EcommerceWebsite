7b2dca55025dd611187f9b322d2595197418076d
Gopal <gopalgawade1295@gmail.com>
2022-10-13 10:13:20 +0530

    first commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { listProducts } from '../redux/actions/ProductActions'
 import { useDispatch, useSelector } from 'react-redux'
-import { Box, Grid, Toolbar, Typography } from '@mui/material'
+import { Box, Grid, Toolbar, Typography, TextField } from '@mui/material'
 
 function HomePage() {
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList)
     const { error, loading, products } = productList
+    const [keyword, setKeyword] = useState('')
 
     useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
+        dispatch(listProducts(keyword))
+    }, [dispatch, keyword])
 
     return (
         <Box sx={{ m: 2 }}>
             <Toolbar sx={{ mb: 3, '@media (max-width: 500px)': { mb: 9 } }} />
+            <Box textAlign='left'>
+                <TextField
+                    id='outlined-size-small'
+                    label='Search'
+                    type='text'
+                    size='small'
+                    value={keyword}
+                    sx={{ mb: 2 }}
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+            </Box>
             {
                 loading ? <Typography variant='caption'>Loading...</Typography> :
                     error ? <Typography variant='caption' sx={{ color: '#FF0000' }}>{error}</Typography> :
